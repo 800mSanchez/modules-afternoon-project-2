68bd9a23f4690da5b4f81deb307f98a3889948bc
Alejandro Sanchez <alejandrosanchez9494@gmail.com>
2020-07-29 18:02:07 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      dropDownVisible: false
+    }
+  }
+
+  toggleDropDown = () => {
+    this.setState({
+      dropDownVisible: !this.state.dropDownVisible
+    })
+  }
+
+    render() {
+        return (
+       <body>
+          <div className ="App">
+                <header className ="main-header">
+                    <img src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg" alt="Start Bootstrap"/>
+                    <nav className="desktop-menu">
+                        <span><a>Services</a></span>
+                        <span><a>Portfolio</a></span>
+                        <span><a>About</a></span>
+                        <span><a>Team</a></span>
+                        <span><a>Contact</a></span>
+                    </nav>
+                    <div className="dropdown" onClick={this.toggleDropDown}>Menu</div>
+                    { this.state.dropDownVisible ? ( 
+                      <nav className="mobile-menu">
+                          <span><a>Services</a></span>
+                          <span><a>Portfolio</a></span>
+                          <span><a>About</a></span>
+                          <span><a>Team</a></span>
+                          <span><a>Contact</a></span>
+                      </nav>
+                      ) : null }
+                </header>
+            </div>
+      </body>
+        )
+    }
 }
 
-export default App;
+export default App
